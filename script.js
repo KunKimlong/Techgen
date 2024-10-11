a8e985e535ee2a54ec073f1b870b193e1b65a2d0
Kimlong KUN <kimlong.kun@allweb.com.kh>
2024-10-11 18:20:53 +0700

    service

--- script.js
+++
@@ -1,6 +0,0 @@
-var header = document.querySelector('header');
-var main   = document.querySelector('main');
-var getHight = header.clientHeight;
-main.style.marginTop = getHight+"px";
-console.log(getHight);
-
