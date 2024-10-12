487ee5b4cbedd839331d35c4a2d87f48db6db861
Kimlong KUN <kimlong.kun@allweb.com.kh>
2024-10-12 16:09:53 +0700

    service page

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,5 +2,4 @@ var header = document.querySelector('header');
 var main   = document.querySelector('main');
 var getHight = header.clientHeight;
 main.style.marginTop = getHight+"px";
-console.log(getHight);
 
---
+++ assets/javascript/service.js
@@ -0,0 +1,27 @@
+const service = [
+    {
+        title:"Moblie App Development",
+        icon:`<i class="fa-light fa-mobile-notch"></i>`,
+        description:"a",
+        listService:[], 
+    },
+    {
+        title:"UX/UI Design",
+        icon:`<i class="fa-sharp fa-regular fa-laptop-file"></i>`,
+        description:"a",
+        listService:[], 
+    },
+    {
+        title:"Web Development",
+        icon:`<i class="fa-sharp fa-solid fa-laptop-code"></i>`,
+        description:"a",
+        listService:[], 
+    },
+    {
+        title:"Ecomerse Development",
+        icon:`<i class="fa-sharp-duotone fa-solid fa-bags-shopping"></i>`,
+        description:"a",
+        listService:[], 
+    },
+
+];
