f541b653e4fd20f4dba96c451045c9d8266f0318
Kimlong KUN <kimlong.kun@allweb.com.kh>
2024-11-08 17:56:47 +0700

    done

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -15,3 +15,32 @@ service.forEach(el=>{
     `;
 })
 serviceDisplay.innerHTML = serviceContent;
+var serviceMore = document.querySelector('.service-more');
+var serviceMoreContent = '';
+service.forEach(el=>{
+    var serviceLi = '';
+    el.listService.forEach(list=>{
+        serviceLi +=`
+            <li>${list}</li>
+        `;
+    })
+    serviceMoreContent+=`
+        <div class="service-more-content">
+            <div class="image">
+                <img src="/assets/image/${el.image}" alt="" width="100%" height="100%">
+            </div>
+            <div class="service-detail">
+                <div class="title">
+                    ${el.title}
+                </div>
+                <div class="description">
+                    ${el.description}
+                </div>
+                <ul class="list">
+                    ${serviceLi}
+                </ul>
+            </div>
+        </div>
+    `;
+})
+serviceMore.innerHTML = serviceMoreContent;
--- assets/javascript/service.js
+++ assets/javascript/service.js
@@ -1,27 +1,31 @@
 const service = [
     {
         title:"Moblie App Development",
+        image:'mobile_image.png',
         icon:`<i class="fa-light fa-mobile-notch"></i>`,
         description:"We always keep an eye on our mobile application design and development because it’s our core service and we know it’s important for your business.",
         listService:["IOS and Adroid development","Native and Cross-platform","Business app","News app","and many type of app"], 
     },
     {
         title:"UX/UI Design",
+        image:'ux-ui.png',
         icon:`<i class="fa-sharp fa-regular fa-laptop-file"></i>`,
         description:"We love beautiful design and we know you do too; that’s why we care so much about our design principle. We want to make your product stand out from the rest.",
         listService:["User experience desgin","User Interface design","Design Web page and Mobile platform"], 
     },
     {
         title:"Web Development",
+        image:'web-image.png',
         icon:`<i class="fa-sharp fa-solid fa-laptop-code"></i>`,
         description:"The full cycle of services to create and launch a website from scratch or redesign a live website along with the modern trends and requirements",
-        listService:[], 
+        listService:["Web app development","Responsive design for mobile","Modern CI/CD tools","Amazon web service (AWS)","Digital ocean"], 
     },
     {
-        title:"Ecomerse Development",
+        title:"E-Commerse Development",
+        image:'E-commerce.png',
         icon:`<i class="fa-sharp fa-regular fa-bags-shopping"></i>`,
-        description:"a",
-        listService:[], 
+        description:"We love E-commerce so we always create an elegant user experience for e-commerce applications/websites that achieve business goals and make users happy.",
+        listService:["Single & multi-store e-commerce","Delivery & location-based e-commerce","Payment online integration","Amazon web service (AWS)"], 
     },
 
 ];
