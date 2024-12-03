1121379c773999121881418941a575e3695e94c7
KunKimlong <kimlongkun2412@gmail.com>
2024-12-03 17:53:02 +0700

    new master

--- assets/javascript/project.js
+++ assets/javascript/project.js
@@ -4,6 +4,31 @@ const project = [
         title:"Artville Digital",
         image:"/assets/image/Artville.jpg",
         group:[service[1].title,service[2].title],
-    }
+    },
+    {
+        title:"Avidia Bank",
+        image:"/assets/image/AvidiaBank.jpg",
+        group:[service[2].title],
+    },
+    {
+        title:"Boo Shop",
+        image:"/assets/image/Boo.jpg",
+        group:[service[2].title,service[3].title],
+    },
+    {
+        title:"Clearbit Technology",
+        image:"/assets/image/Clearbit.png",
+        group:[service[2].title],
+    },
+    {
+        title:"Delizioso Resturant",
+        image:"/assets/image/Delizioso.jpg",
+        group:[service[1].title,service[2].title],
+    },
+    {
+        title:"Solutions Technology",
+        image:"/assets/image/solutions.jpg",
+        group:[service[2].title],
+    },
 
 ];
